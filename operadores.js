7afd09f30b7776100dd1d1d98a1ad31298651a45
Carlos <carlosgomezxdc@gmai.com>
2022-11-28 11:58:02 -0500

    first commit

---
+++ operadores.js
@@ -0,0 +1,32 @@
+let a,b,c,d,e,f =0
+
+//Ejercicio 1
+a=40+2*7**2
+b=3+ (2+5)*1+ (3-2*3)
+c=16 - 8 * (10 - (5 + 4))
+d=(5+3*2)-(7*2)*(3+ (2*3-1))
+e=(24+220)/3*2
+f=(6/3*2)-(7*2)+ (2*(3+ (2/3-1)*4))
+
+console.log(a,b,c,d,e,f)
+
+//Ejercicio 2
+
+
+a=29.7 + 5.0 ** 2.0
+b=( (2 - 3) ** 4 * 5 / (4 + 3 * 9) )
+c=49.38 + 127.73 - 15.02 * 6.83 / 3.22
+d=19 / 3
+e=15 % 6
+f=((15 * 8 % 6 + 24 / 2 ** 3) ** 3 / 4) * (5 ** 1 / 2 + 1 / 4 + 2 ** 3 - 4)
+
+console.log(a,b,c,d,e,f)
+
+//Ejercicio 3
+
+a= true || true && false || true
+b= !( (false || false) && (true || true) || true )
+c= ! false && (false || true) && (! true && false)
+d= ! ( true || false) && (false && true)
+
+console.log(a,b,c,d)
